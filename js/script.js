d67303145a0663557dcbf70069f1d2abe51c0d48
happyGGang <joy.gang@dxworks.kr>
2022-10-20 20:23:04 +0900

    [update] 시작 기능 구현

--- js/script.js
+++ js/script.js
@@ -4,4 +4,41 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  class StopWatch {
+    constructor (element) {
+      this.timer = element
+      this.interval = null
+      this.defaultTime = '00:00:00'
+      this.time = 0
+      this.elapsedTime = 0
+    }
+
+    timeToString(time) {
+      const date = new Date(time)
+      const minutes = date.getUTCMinutes
+      const seconds = date.getUTCSeconds
+        const milliseconds = date.getUTCMilliseconds
+    }
+
+    startTimer() {
+      this.elapsedTime = Date.now() - this.startTime
+      const time = this.timeToString(this.elapsedTime)
+    }
+
+    start () {
+      this.startTime = Date.now() - this.elapsedTime
+      this.interval = setInterval( this.startTimer.bind(this), 10)
+    }
+    stop () {}
+    reset () {}
+  }
+
+  const $startButton = get('.timer_button.start')
+  const $timer = get('.timer')
+  const stopWatch = new StopWatch($timer)
+
+  $startButton.addEventListener('click', () => {
+    stopWatch.start()
+  })
 })()
